d9c74648a24cdb7104b8794da9dbaee28ff0c77a
Quan <qhoang2373@gmail.com>
2024-09-24 21:20:45 -0700

    add foodschema

--- models/user.js
+++ models/user.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
 
+const foodSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  }
+});
+
 const userSchema = mongoose.Schema({
   username: {
     type: String,
