9442ffd311ea6906772118e02105edae228d1ec4
Quan <qhoang2373@gmail.com>
2024-09-24 21:38:05 -0700

    import food controllers in server.js and use middle ware

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
 
+
 const authController = require('./controllers/auth.js');
+const foodsController = require('./controllers/foods.js');
 
 const port = process.env.PORT ? process.env.PORT : '3000';
 
@@ -16,6 +18,9 @@ mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
+
+app.use('/auth', authController);
+app.use('/users/:userId/foods',foodsController);
 
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
