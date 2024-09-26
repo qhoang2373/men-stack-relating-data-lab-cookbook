eb4dfaac93dae5e3d33668163d22f05e76a60bd9
Quan <qhoang2373@gmail.com>
2024-09-25 21:31:47 -0700

    add create and index routes in food.js

--- controllers/foods.js
+++ controllers/foods.js
@@ -1,15 +1,42 @@
 
+//===============inputs================//
 const express = require('express');
 const router = express.Router();
-
 const User = require('../models/user.js');
 
-router.get('/', async(req, res) => {
-  res.render('foods/index.ejs')
+//===============Routes================//
+// 
+router.get('/new', async(req, res) => {
+  res.render('foods/new.ejs')
 });
 
-router.get('/new', async(req, res) => {
-    res.render('foods/new.ejs')
-  });
+// Index Route
+router.get('/', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);   
+    res.render('foods/index.ejs', {
+      foods: currentUser.pantry,
+    })
+  } catch (error) {
+    console.log(error)
+    res.redirect('/')
+  }
+});
+
+  // Create Route
+router.post('/', async (req, res) => {
+    try {
+        
+      const currentUser = await User.findById(req.session.user._id);
+        currentUser.pantry.push(req.body)
+        
+        await currentUser.save()
+        res.redirect(`/users/${currentUser._id}/foods/`);
+      } catch (error) {
+        console.log(error);
+        res.redirect('/')
+      }
+    });
   
+    
 module.exports = router;
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//===============inputs================//
 const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
@@ -17,13 +18,14 @@ const passUserToView = require('./middleware/pass-user-to-view.js');
 
 const port = process.env.PORT ? process.env.PORT : '3000';
 
-mongoose.connect(process.env.MONGODB_URI);
+//===============Mongoose================//
 
+mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
-
+//===============Middleware=================//
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 app.use(morgan('dev'));
@@ -35,6 +37,7 @@ app.use(
   })
 );
 
+//===============Routes================//
 app.get('/', (req, res) => {
   res.render('index.ejs', {
     user: req.session.user,
@@ -46,16 +49,17 @@ app.get('/foods', (req,res) => {
 })
 
 app.get('/foods/new', (req, res) => {
-  res.render('foods/new.ejs'); 
+  const user = req.session.user;
+  res.render('foods/new.ejs', { user });
 });
 
-app.get('/vip-lounge', (req, res) => {
-  if (req.session.user) {
-    res.send(`Welcome to the party ${req.session.user.username}.`);
-  } else {
-    res.send('Sorry, no guests allowed.');
-  }
-});
+// app.get('/vip-lounge', (req, res) => {
+//   if (req.session.user) {
+//     res.send(`Welcome to the party ${req.session.user.username}.`);
+//   } else {
+//     res.send('Sorry, no guests allowed.');
+//   }
+// });
 
 app.use(passUserToView)
 app.use('/auth', authController);
